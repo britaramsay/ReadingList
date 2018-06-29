da0565d1f615535db064eed1392b5148e9c7aaf4
Brita Ramsay <britaramsay@gmail.com>
2018-06-28 21:14:19 -0400

    finished verison

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,20 @@ import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => (
+  <Router>
   <div>
     <Nav />
-    <Books />
+    <Switch>
+      <Route exact path="/" component={Books} />
+      <Route exact path="/books" component={Books} />
+      <Route path="/books/:id" component={Detail} />
+      <Route component={NoMatch} />
+    </Switch>
   </div>
+  </Router>
 );
 
-export default App;
+export default App; 
